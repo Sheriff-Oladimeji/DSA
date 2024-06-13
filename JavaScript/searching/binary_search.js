4b63c1c9d831e53f67c662b073b7cba2b421a039
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-06-13 16:43:12 +0100

    fix: add binary search for js

---
+++ JavaScript/searching/binary_search.js
@@ -0,0 +1,25 @@
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    midpoint = Math.floor((left + right) / 2);
+    if (arr[midpoint] === target) {
+      return midpoint;
+    } else if (arr[midpoint] < target) {
+      left = midpoint + 1;
+    } else {
+      right = midpoint - 1;
+    }
+  }
+  return -1;
+};
+
+const myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+const myTarget = 15;
+const result = binarySearch(myArray, myTarget);
+
+if (result !== -1) {
+  console.log(`Value ${myTarget} found at index ${result}`);
+} else {
+  console.log("Target not found in array.");
+}
