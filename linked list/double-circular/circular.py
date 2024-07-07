17840afb6cb6e8bfce2a801cffc2d8b7a429b74b
Sheriff  Oladimeji <dimejiademola5@gmail.com>
2024-07-07 16:04:58 +0100

    feat: add double circular.py

---
+++ linked list/double-circular/circular.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+node1 = Node(3)
+node2 = Node(5)
+node3 = Node(13)
+node4 = Node(2)
+
+node1.next = node2
+node1.prev = node4
+
+node2.prev = node1
+node2.next = node3
+
+node3.prev = node2
+node3.next = node4
+
+node4.prev = node3
+node4.next = node1
+
+print("\nTraversing forward:")
+currentNode = node1
+startNode = node1
+print(currentNode.data, end=" -> ")
+currentNode = currentNode.next
+
+while currentNode != startNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+print("...")
+
+print("\nTraversing backward:")
+currentNode = node4
+startNode = node4
+print(currentNode.data, end=" -> ")
+currentNode = currentNode.prev
+
+while currentNode != startNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.prev
+print("...")
